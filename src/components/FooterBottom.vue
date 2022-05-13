0bc28331b9058aaee6e62b49bf0dc40f4d4bec82
agostinopique <ago.pique@yahoo.it>
2022-05-13 18:16:31 +0200

    Aggiunto contenitore sezione footer bottom;f

--- src/components/FooterBottom.vue
+++ src/components/FooterBottom.vue
@@ -1,13 +1,13 @@
 <template>
-  
+    <div class="container"></div>
 </template>
 
 <script>
 export default {
-
+    name: 'FooterBottom'
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
 </style>
